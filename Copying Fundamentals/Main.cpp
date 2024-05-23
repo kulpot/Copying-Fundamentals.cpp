26ba080c641b26c42d365457b11e45bf2adad5e1
Kulpot <kulpotwahu@gmail.com>
2024-05-23 18:53:19 +0800

    Main.cpp-> //Copying Fundamentals | C++ Tutorials //ref link:https://www.youtube.com/watch?v=yXObZfT_e4w&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a

--- Copying Fundamentals/Main.cpp
+++ Copying Fundamentals/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+//Copying Fundamentals | C++ Tutorials //ref link:https://www.youtube.com/watch?v=yXObZfT_e4w&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a
+
 int main() {
 
 	return 0;
