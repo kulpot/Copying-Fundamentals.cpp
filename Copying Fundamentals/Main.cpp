a9e99c9a936d3e84060a07fd83d5efbe08896d10
Kulpot <kulpotwahu@gmail.com>
2024-05-23 18:55:21 +0800

    Main.cpp->
    int main() {
     int x = 1;
     int y = x;
    
     std::cout << "x = " << x << '\n';
     std::cout << "y = " << y << '\n';

--- Copying Fundamentals/Main.cpp
+++ Copying Fundamentals/Main.cpp
@@ -3,6 +3,11 @@
 //Copying Fundamentals | C++ Tutorials //ref link:https://www.youtube.com/watch?v=yXObZfT_e4w&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a
 
 int main() {
+	int x = 1;
+	int y = x;
+
+	std::cout << "x = " << x << '\n';
+	std::cout << "y = " << y << '\n';
 
 	return 0;
 }
