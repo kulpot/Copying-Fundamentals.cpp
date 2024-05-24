5a12a1d3e2d3e0c06b79986816d99cda74389baf
Kulpot <kulpotwahu@gmail.com>
2024-05-24 17:35:26 +0800

    Main.cpp->
    void square(int num) {
     num *= num;
    }
    
    int main() {
     int x = 5;
     square(x);
    
     std::cout << "x = " << x << '\n';

--- Copying Fundamentals/Main.cpp
+++ Copying Fundamentals/Main.cpp
@@ -3,13 +3,14 @@
 //Copying Fundamentals | C++ Tutorials //ref link:https://www.youtube.com/watch?v=yXObZfT_e4w&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a
 
 void square(int num) {
-
+	num *= num;
 }
 
 int main() {
+	int x = 5;
+	square(x);
 
-
-
+	std::cout << "x = " << x << '\n';
 
 
 
